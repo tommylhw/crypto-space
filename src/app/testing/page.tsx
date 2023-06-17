2d017dbf0a2253036f0effed0b64f3de80a4227e
tommylhw <135199972+TommyWongDev@users.noreply.github.com>
2023-06-17 21:47:06 +0800

    updated

--- src/app/testing/page.tsx
+++ src/app/testing/page.tsx
@@ -1,25 +1,25 @@
-'use client'
-import React from 'react'
-import dynamic from "next/dynamic";
-import { useMediaQuery } from 'react-responsive'
+// 'use client'
+// import React from 'react'
+// import dynamic from "next/dynamic";
+// import { useMediaQuery } from 'react-responsive'
 
-// Components
-import MyDatePicker from '@components/MyDatePicker';
+// // Components
+// import MyDatePicker from '@components/MyDatePicker';
 
-const DynamicDatepicker = dynamic(() => import("@components/MyDatePicker"), { ssr: false });
+// const DynamicDatepicker = dynamic(() => import("@components/MyDatePicker"), { ssr: false });
 
-const TestingPage = () => {
+// const TestingPage = () => {
 
-  const isMobile = useMediaQuery({ query: '(max-width: 520px)' });
+//   const isMobile = useMediaQuery({ query: '(max-width: 520px)' });
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <MyDatePicker />
-      <DynamicDatepicker />
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-center p-24">
+//       <MyDatePicker />
+//       <DynamicDatepicker />
 
-      {isMobile ? <h1>Mobile</h1> : <h1>Desktop</h1>}
-    </main>
-  )
-}
+//       {isMobile ? <h1>Mobile</h1> : <h1>Desktop</h1>}
+//     </main>
+//   )
+// }
 
-export default TestingPage;
+// export default TestingPage;
